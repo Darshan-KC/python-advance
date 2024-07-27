43c6942a5f634e02ebac2eae6bff69adf0271160
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-07-27 20:55:09 +0545

    fibonacci using pascal's triangle added

---
+++ fibonacci-using-pascal-triangel.py
@@ -0,0 +1,18 @@
+# The Fibonacci numbers are hidden inside of Pascal's triangle. If you sum up the _ numbers of the following triangle, you will get the 7th Fibonacci number:
+
+
+#            1
+
+#           1   1
+
+#          1   2   1
+
+#        1   3   3  _1
+
+#      1   4  _6   4   1
+
+#    1  _5   10  10   5   1
+
+# _1    6   15  20   15   6   1
+
+# Write a recursive program to calculate the Fibonacci numbers, using Pascal's triangle.
